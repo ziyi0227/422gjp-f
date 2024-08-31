8b18652371983b95fabdc24c59aa9932aa062552
ziyi0227 <1206881089@qq.com>
2024-08-31 12:54:32 +0800

    分析窗口

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -49,15 +49,14 @@
       </el-row>
     </el-card>
     <el-card>
-      <el-progress :percentage="100" :format="format"></el-progress>
+      <el-progress :percentage="100" :format="format" />
     </el-card>
     <el-row :gutter="20">
       <el-col :span="12">
         <el-card>
           <div slot="header" class="clearfix">
             <span>收入统计</span>
-            <!--TODO: 抽屉，分析-->
-            <el-button style="float: right; padding: 3px 0" type="text">分析></el-button>
+            <el-button style="float: right; padding: 3px 0" type="text" @click="incomeDrawer">分析></el-button>
           </div>
           <div>
             <LineChart />
@@ -71,8 +70,20 @@
         <el-card>
           <div slot="header" class="clearfix">
             <span>支出统计</span>
-            <!--TODO: 抽屉，分析-->
-            <el-button style="float: right; padding: 3px 0" type="text">分析></el-button>
+            <el-button style="float: right; padding: 3px 0" type="text" @click="outcomeDrawer">分析></el-button>
+            <el-drawer
+              :title="drawerTitle"
+              :visible.sync="drawer"
+              :direction="direction"
+              :before-close="handleClose">
+              <!-- 使用带有滚动条的容器包裹评语内容 -->
+              <div class="drawerText">
+                <el-scrollbar style="height: 100%; margin-outside: 3px">
+                  <VueMarkdown v-if="drawerType === 'income'" :source="evaltext.incomeText" />
+                  <VueMarkdown v-else :source="evaltext.outcomeText" />
+                </el-scrollbar>
+              </div>
+            </el-drawer>
           </div>
           <div>
             <LineChartOut />
@@ -94,20 +105,23 @@
           </el-col>
           <el-col :span="6">
             <span>分析</span>
-            <el-divider direction="vertical"></el-divider>
-          <!--  TODO:markdown-->
+            <el-divider direction="vertical" />
+            <!-- 使用带有滚动条的容器包裹评语内容 -->
+            <el-scrollbar style="height: 100%; margin-outside: 3px">
+              <VueMarkdown :source="evaltext.commend" />
+            </el-scrollbar>
           </el-col>
         </el-row>
-        <el-divider></el-divider>
+        <el-divider />
         <el-row :gutter="10">
           <el-col :span="8">
-            <NightingaleChart></NightingaleChart>
+            <NightingaleChart />
           </el-col>
           <el-col :span="8">
-            <AccessFrom></AccessFrom>
+            <AccessFrom />
           </el-col>
           <el-col :span="8">
-            <CircleAccessFrom></CircleAccessFrom>
+            <CircleAccessFrom />
           </el-col>
         </el-row>
       </div>
@@ -122,9 +136,10 @@ import BingChart from '@/views/dashboard/bingChart'
 import BingChartOut from '@/views/dashboard/bingChartOut'
 import LineChartOut from '@/views/dashboard/LineChartOut'
 import AreaChart from '@/views/dashboard/AreaChart'
-import NightingaleChart from "@/views/dashboard/NightingaleChart";
-import AccessFrom from "@/views/dashboard/AccessFrom";
-import CircleAccessFrom from "@/views/dashboard/CircleAccessFrom";
+import NightingaleChart from '@/views/dashboard/NightingaleChart'
+import AccessFrom from '@/views/dashboard/AccessFrom'
+import CircleAccessFrom from '@/views/dashboard/CircleAccessFrom'
+import VueMarkdown from 'vue-markdown'
 
 export default {
   name: 'Dashboard',
@@ -136,6 +151,7 @@ export default {
     AreaChart,
     NightingaleChart,
     AccessFrom,
+    VueMarkdown,
     CircleAccessFrom
   },
   computed: {
@@ -147,12 +163,39 @@ export default {
     return {
       memberNumber: 5,
       totalMoney: 120000.00,
-      increaseMoney: 5000
+      increaseMoney: 5000,
+      evaltext: {
+        assess: '',
+        commend: '111测试**粗**',
+        incomeText: '**income**',
+        outcomeText: '**outcome**'
+      },
+      drawer: false,
+      direction: 'rtl',
+      drawerType: '',
+      drawerTitle: ''
     }
   },
   methods: {
     format(percentage) {
       return percentage === 100 ? '超预算' : `${percentage}%`
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    outcomeDrawer() {
+      this.drawer = true
+      this.drawerType = 'outcome'
+      this.drawerTitle = '支出分析'
+    },
+    incomeDrawer() {
+      this.drawer = true
+      this.drawerType = 'income'
+      this.drawerTitle = '收入分析'
     }
   }
 }
@@ -170,5 +213,8 @@ export default {
 }
 .dashboard-container .el-card{
   margin-bottom: 20px;
+}
+.drawerText{
+  margin: 20px;
 }
 </style>
